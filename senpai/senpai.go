c4691698c5adee5453ec06340d8ddc39485b9d7c
Rizwan Tejpar <natural-affinity@users.noreply.github.com>
2019-01-11 23:40:08 -0800

    re-arrange functions for readability

--- senpai/senpai.go
+++ senpai/senpai.go
@@ -13,18 +13,6 @@ type Task struct {
 	Interval time.Duration
 }
 
-// Dispatch command and fetch results
-func Dispatch(cmd string) (string, error) {
-	command := exec.Command("sh", "-c", cmd)
-	out, err := command.CombinedOutput()
-
-	if err != nil {
-		return string(out), err
-	}
-
-	return string(out), nil
-}
-
 // Monitor task and
 func (t *Task) Monitor() (string, error) {
 	for {
@@ -36,4 +24,16 @@ func (t *Task) Monitor() (string, error) {
 		fmt.Printf(out)
 		time.Sleep(t.Interval)
 	}
+}
+
+// Dispatch command and fetch results
+func Dispatch(cmd string) (string, error) {
+	command := exec.Command("sh", "-c", cmd)
+	out, err := command.CombinedOutput()
+
+	if err != nil {
+		return string(out), err
+	}
+
+	return string(out), nil
 }
